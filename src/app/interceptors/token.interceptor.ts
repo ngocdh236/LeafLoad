d5f110f3b6cb3d4ba0d3fa2d3cb9f72802527114
Thinh Vo <ducthinh2410@gmail.com>
2018-02-25 22:07:53 +0200

    Cleaned up interceptor

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -15,7 +15,6 @@ export class TokenInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // TODO: Create an AuthService to encapsulate the token-related manipulations
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxNywidXNlcm5hbWUiOiJ0aGluaHZvIiwiZW1haWwiOiJ0aGluaHZAbWV0cm9wb2xpYS5maSIsImZ1bGxfbmFtZSI6bnVsbCwiaXNfYWRtaW4iOm51bGwsInRpbWVfY3JlYXRlZCI6IjIwMTgtMDEtMjlUMTA6NTY6MDEuMDAwWiIsImlhdCI6MTUxOTA0NjEwMiwiZXhwIjoxNTIxMTE5NzAyfQ.K2_RI2NM4g0Pdjn44v-CvSol0VdMkZa-n8XZ4L2JCak';//localStorage.getItem('token');
-    console.log(`This is access token: ${token}`);
     if (token && token.trim() !== '') {
       request = request.clone({
         setHeaders: {
